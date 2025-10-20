dcf42242ea9feb7f78275c93b70b947d20f2969e
bearxiao9756 <bearxiao9@gmail.com>
2025-10-20 20:43:17 +0800

    Update prod.env.ts

--- config/prod.env.ts
+++ config/prod.env.ts
@@ -1,7 +1,7 @@
-const BASE_DOMAIN = '43.160.198.172'
-const CHAT_URL = `http://${BASE_DOMAIN}:10008`
-const API_URL = `http://${BASE_DOMAIN}:10002`
-const WS_URL = `ws://${BASE_DOMAIN}:10001`
+const BASE_DOMAIN = 'acone.icu'
+const CHAT_URL = `https://${BASE_DOMAIN}/chat`
+const API_URL = `https://${BASE_DOMAIN}/api`
+const WS_URL = `wss://${BASE_DOMAIN}/msg_gateway`
 
 
 export default {
