8df33d2e27d473178bb12ba535d4bcda28265786
bearxiao9756 <bearxiao9@gmail.com>
2025-10-24 10:08:35 +0800

    更换域名 aike002.icu

--- src/pages/login/index/index.vue
+++ src/pages/login/index/index.vue
@@ -237,10 +237,10 @@ const name = [
   '乐开颜'
 ];
 
-const imge = ['https://kefu.hfone.icu/api/object/1246839606/ic_avatar_06.webp',
-      'https://kefu.hfone.icu/api/object/9653164283/1761021086245png',
-      "https://kefu.hfone.icu/api/object/1246839606/ic_avatar_03.webp",
-      "https://kefu.hfone.icu/api/object/1246839606/ic_avatar_02.webp",
+const imge = ['https://kefu.aike002.icu/api/object/1246839606/ic_avatar_06.webp',
+      'https://kefu.aike002.icu/api/object/9653164283/1761021086245png',
+      "https://kefu.aike002.icu/api/object/1246839606/ic_avatar_03.webp",
+      "https://kefu.aike002.icu/api/object/1246839606/ic_avatar_02.webp",
       ]
 const onchaliAuto = async () => {
 
