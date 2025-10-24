cd924d6a5182f582cf92114db50a9b625eb1d10c
bearxiao9756 <bearxiao9@gmail.com>
2025-10-24 15:20:12 +0800

    acone 候补地址

--- src/pages/login/index/index.vue
+++ src/pages/login/index/index.vue
@@ -237,10 +237,10 @@ const name = [
   '乐开颜'
 ];
 
-const imge = ['https://web.acone.icu/api/object/1246839606/ic_avatar_06.webp',
-      'https://kefu.acone.icu/api/object/9653164283/1761021086245png',
-      "https://web.acone.icu/api/object/1246839606/ic_avatar_03.webp",
-      "https://web.acone.icu/api/object/1246839606/ic_avatar_02.webp",
+const imge = ['https://hao.acone.icu/api/object/1246839606/ic_avatar_06.webp',
+      'https://hao.acone.icu/api/object/9653164283/1761021086245png',
+      "https://hao.acone.icu/api/object/1246839606/ic_avatar_03.webp",
+      "https://hao.acone.icu/api/object/1246839606/ic_avatar_02.webp",
       ]
 const onchaliAuto = async () => {
 
