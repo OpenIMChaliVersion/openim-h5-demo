bce325ec2ceee89307dcfa89be01f00e8516e079
bearxiao9756 <bearxiao9@gmail.com>
2025-10-25 19:36:32 +0800

    Update index.vue

--- src/pages/login/index/index.vue
+++ src/pages/login/index/index.vue
@@ -236,11 +236,20 @@ const name = [
   '大成功',
   '乐开颜'
 ];
-const imge = [
-      'https://kefu.kfhao.icu/api/object/1246839606/ic_avatar_06.webp',
-      'https://kefu.kfhao.icu/api/object/9653164283/1761021086245png',
-      "https://kefu.kfhao.icu/api/object/1246839606/ic_avatar_03.webp",
-      "https://kefu.kfhao.icu/api/object/1246839606/ic_avatar_02.webp"]
+// const imge = [
+//       'https://kefu.kfhao.icu/api/object/1246839606/ic_avatar_06.webp',
+//       'https://kefu.kfhao.icu/api/object/9653164283/1761021086245png',
+//       "https://kefu.kfhao.icu/api/object/1246839606/ic_avatar_03.webp",
+//       "https://kefu.kfhao.icu/api/object/1246839606/ic_avatar_02.webp"]
+
+const imge = ['https://web.kfhao.icu/api/object/1801995308/ic_avatar_01.webp',
+      'https://web.kfhao.icu/api/object/1801995308/ic_avatar_02.webp',
+      "https://kefu.kfhao.icu/api/object/1801995308/ic_avatar_03.webp",
+      "https://kefu.kfhao.icu/api/object/1801995308/ic_avatar_04.webp",
+      "https://kefu.kfhao.icu/api/object/1801995308/ic_avatar_05.webp",
+      "https://kefu.kfhao.icu/api/object/1801995308/ic_avatar_06.webp",
+      "https://kefu.kfhao.icu/api/object/1801995308/user.webp",
+]
 
 
 const onchaliAuto = async () => {
