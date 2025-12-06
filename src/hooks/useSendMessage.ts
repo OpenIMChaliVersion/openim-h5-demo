4f9ead076ad164bf887c8e192f280ae8b27a86c2
bearxiao9756 <bearxiao9@gmail.com>
2025-12-06 14:54:07 +0800

    消息发送失败提示

--- src/hooks/useSendMessage.ts
+++ src/hooks/useSendMessage.ts
@@ -6,6 +6,7 @@ import { IMSDK } from '@/utils/imCommon'
 import type { MessageItem } from '@openim/client-sdk/lib/types/entity'
 import { MessageStatus } from '@openim/client-sdk'
 import { SendMsgParams } from '@openim/client-sdk/lib/types/params'
+import { feedbackToast } from '@/utils/common'
 
 const messageStore = useMessageStore()
 const conversationStore = useConversationStore()
@@ -42,13 +43,15 @@ export default function useSendMessage() {
       // @ts-ignore
       const { data: successMessage } = await IMSDK.sendMessage(options)
       messageStore.updateOneMessage(successMessage as ExMessageItem, true)
+      feedbackToast({ message: "消息发送成功"})
     } catch (error) {
       console.error(error)
-
+      
       messageStore.updateOneMessage({
         ...message,
         status: MessageStatus.Failed,
       })
+      feedbackToast({ message: "消息发送失败", error })
     }
   }
 
